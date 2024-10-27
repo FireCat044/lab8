f2ed6f8e961eae7557ad190ae480989723b0e543
Горбунов Андрій <andreigorbunov044@gmail.com>
2024-10-27 21:22:12 +0800

    Добавьте файлы проекта.

---
+++ lab8/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using lab8.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab8.Controllers
+{
+    public class ModelController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { ID = 1, Name = "IPhone", Price = 1000f, CreatedDate = DateTime.Now.AddDays(-5) },
+                new Product { ID = 2, Name = "MacBook", Price = 2000f, CreatedDate = DateTime.Now.AddDays(-7) },
+                new Product { ID = 3, Name = "IMac", Price = 2500f, CreatedDate = DateTime.Now }
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ lab8/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace lab8.Models
+{
+        public class Product
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public float Price { get; set; }
+            public DateTime CreatedDate { get; set; }
+        }
+}
